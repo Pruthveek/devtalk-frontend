5f4951ead91f758873ee927c22bc82f3122e663b
Pruthveek <pruthveek512@gmail.com>
2025-10-09 14:33:00 +0530

    payment

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -67,6 +67,23 @@ const Navbar = () => {
             >
               <div className="w-10 rounded-full">
                 <img alt="user profile photo" src={user.photoUrl} />
+                {user?.isPremium && (<>
+                <div className="absolute -top-2 -right-2 rounded-full animate-ping">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill={user?.membershipType==="gold"?"gold":"silver"}
+                    viewBox="0 0 24 24"
+                    className="inline-block w-4 h-4 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="0"
+                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.54 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.784.57-1.838-.197-1.539-1.118l1.518-4.674a1 1 0 00-.363-1.118L2.98 10.1c-.783-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    ></path>
+                  </svg>
+                </div>
+                </>)}
               </div>
             </div>
             <ul
